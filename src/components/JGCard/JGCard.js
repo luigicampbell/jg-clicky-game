9a4600c801e539f8eb6c40bb8199e43d14ffbc1c
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-04-15 21:49:41 -0700

    Changed function name from select to choose

--- src/components/JGCard/JGCard.js
+++ src/components/JGCard/JGCard.js
@@ -4,8 +4,8 @@ import "./JGCard.css";
 const JGCard = props => (
     <div className="card">
         <div className="img-container">
-            <a onClick={() => props.selectJG(props.pose)}
-                className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+            <a onClick={() => props.chooseJG(props.pose)}
+                className={props.score === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
             >
                 <img alt={props.pose} src={props.image} />
             </a>
